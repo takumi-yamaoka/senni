c2ddae3c3c609f0fbce96628c82874de4a7b9991
takumi.yamaoka <tk-y@docomo.ne.jp>
2020-07-30 14:52:10 +0900

    Initial Commit

---
+++ Swift5senni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5senni
+//
+//  Created by 山岡巧 on 2020/07/30.
+//  Copyright © 2020 takumi.yamaoka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
